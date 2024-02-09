c1f71c7211e7f9a3d672852b70bb8ac215073146
HiteshHonmane <hiteshhonmane@gmail.com>
2024-02-09 21:39:02 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className="main-section ">
+
+      <h1 className="heading-section text-6xl font-Valencia ">
+        Hitesh Honmane
+      </h1>
+      
+
+
+     </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Fonts/Brucken/README.txt
@@ -0,0 +1,6 @@
+To learn more about the font family and its license, visit https://www.fontmirror.com/brucken
+
+License: Free for personal use.
+Contact Letterhend@gmail.com for commercial use.
+You can donate to the font author at https://www.paypal.com/paypalme/payslab.
+ 
