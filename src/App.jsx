50edd09d51510f7a5df76b16c2f7718747623bc7
HiteshHonmane <hiteshhonmane@gmail.com>
2024-02-12 15:54:34 +0530

    Added Light and Dark mode using Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 import Hero from './Components/Hero'
 import About from './Components/About'
+import ThemeBtn from './Components/ThemeBtn'
+import { ThemeProvider } from './Contexts/Theme'
+import Themchanger from './Components/Themchanger'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [themeMode, setThemeMode] = useState("light")
+
+ const lightThem = ()=>{
+  setThemeMode ("light")
+ }
+
+ const darkTheme = ()=>{
+  setThemeMode("dark")
+
+ }
+
+ //Them Change 
+let html = document.querySelector('html')
+useEffect(()=>{
+   html.classList.remove('light', 'dark')
+   html.classList.add(themeMode)
+},[themeMode])
+
 
   return (
-    <>
+    <ThemeProvider value={{themeMode, lightThem, darkTheme}}>
+    <Themchanger/>
       <Hero/>
+      
       <About/>
-    </>
+    </ThemeProvider>
   )
 }
 
