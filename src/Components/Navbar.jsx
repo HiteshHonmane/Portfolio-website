925b35129c96ec51baace7e6091468df3e1ba4a3
HiteshHonmane <hiteshhonmane@gmail.com>
2024-02-28 23:19:33 +0530

    Added svg 's updated navbar style

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ import { GrProjects } from "react-icons/gr";
 function Navbar (){
 
     return(
-        <div className=' absolute flex flex-col w-9 gap-4 ml-4 px-4  border-12  '>
+        <div className=' fixed top-10 w-16  z-20  flex flex-col  gap-4 ml-4 px-4 border-gray-600 bg-[rgba(203,200,200,0.41)] rounded-xl p-7 shadow-black   '>
             <div className='w-9 ' id="Home">
                 <img className='w-11'  src="src\assets\Nav Icons\Profile.svg" alt="" /></div>
             <div className='w-9  ' id='about'>
